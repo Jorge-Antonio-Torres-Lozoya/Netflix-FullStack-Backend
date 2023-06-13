6675d0b4f6492c80b26fd56636020f9fc973cefd
Jorge-Antonio-Torres-Lozoya <jtorres1@ucol.mx>
2023-06-12 21:41:18 -0600

    fix cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import server from "./server.js"
 import { connect } from "./database.js";
 import dotenv from 'dotenv';
+import cors from 'cors';
 dotenv.config();
 connect();
 
+// Habilitar CORS
+const corsOptions = {
+    origin: 'http://localhost:5173', // Cambia esto con tu URL de origen permitido
+    credentials: true, // Si necesitas permitir el envío de cookies o encabezados de autorización
+  };
+  server.express.use(cors(corsOptions));
 server.start( { port: 3000}, ( { port } ) => {
     console.log('Sever is running on ' + port )
 })
