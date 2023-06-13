272a72a5e94f258030493eccc8225222c9de0628
Jorge-Antonio-Torres-Lozoya <jtorres1@ucol.mx>
2023-06-13 17:05:08 -0600

    fix cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import server from "./server.js"
-import { connect } from "./database.js";
-import dotenv from 'dotenv';
-// import cors from 'cors';
-dotenv.config();
-connect();
+// import server from "./server.js"
+// import { connect } from "./database.js";
+// import dotenv from 'dotenv';
+// // import cors from 'cors';
+// dotenv.config();
+// connect();
 
-// Habilitar CORS
-// server.use( cors() );
-server.start( { port: 3000}, ( { port } ) => {
-    console.log('Sever is running on ' + port )
-})
+// server.start( { port: 3000}, ( { port } ) => {
+//     console.log('Sever is running on ' + port )
+// })
+
+
+import { GraphQLServer } from 'graphql-yoga';
+import express from 'express';
+import { typeDefs } from './graphql/schema.graphql';
+import resolvers from "./graphql/index.js"
+
+const app = express();
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start({ app });
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado en el puerto 3000');
+});
--- src/server.js
+++ src/server.js
@@ -2,7 +2,6 @@
 // import { GraphQLServer} from "graphql-yoga";
 // import path from "path"
 // import { fileURLToPath } from 'url';
-// import cors from 'cors'
 // //------
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname( __filename );
@@ -15,34 +14,6 @@
 //     typeDefs: path.join( __dirname, "graphql/schema.graphql" ),  // -> /c/Users/carlf/Desktop/b17-web/graphql/schema.graphql //http:heroku-app.com/graphql/schema.graphql"
 //     resolvers: resolvers                                                     
 // } )
-// server.express.use(cors({
-//     origin: '*', // Configura aquí los orígenes permitidos, ej. 'http://localhost:3000'
-//     methods: ['GET', 'POST'], // Configura los métodos permitidos
-//     allowedHeaders: '*', // Configura '*' para permitir todos los encabezados
-//   }));
 
 // export default server
-import resolvers from "./graphql/index.js";
-import { GraphQLServer } from "graphql-yoga";
-import path from "path";
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-console.log(path.join(__dirname, "graphql/schema.graphql"));
-
-const allowCrossOrigin = (req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-};
-
-const server = new GraphQLServer({
-  typeDefs: path.join(__dirname, "graphql/schema.graphql"),
-  resolvers
-});
-
-server.express.use(allowCrossOrigin);
-
-export default server;
