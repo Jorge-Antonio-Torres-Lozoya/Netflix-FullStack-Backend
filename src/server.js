bd73ecda59426710ad2d06765d56a458b82e6b0a
Jorge-Antonio-Torres-Lozoya <jtorres1@ucol.mx>
2023-06-13 14:38:28 -0600

    fix cors

--- src/server.js
+++ src/server.js
@@ -1,24 +1,50 @@
-import resolvers from "./graphql/index.js"
-import { GraphQLServer} from "graphql-yoga";
-import path from "path"
+// import resolvers from "./graphql/index.js"
+// import { GraphQLServer} from "graphql-yoga";
+// import path from "path"
+// import { fileURLToPath } from 'url';
+// import cors from 'cors'
+// //------
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname( __filename );
+// console.log( path.join( __dirname, "graphql/schema.graphql" ))
+// //-----
+// //relativas    /b17-web
+
+// //absolutas   /c/Users/carlf/Desktop/b17-web
+// const server = new GraphQLServer( {
+//     typeDefs: path.join( __dirname, "graphql/schema.graphql" ),  // -> /c/Users/carlf/Desktop/b17-web/graphql/schema.graphql //http:heroku-app.com/graphql/schema.graphql"
+//     resolvers: resolvers                                                     
+// } )
+// server.express.use(cors({
+//     origin: '*', // Configura aquí los orígenes permitidos, ej. 'http://localhost:3000'
+//     methods: ['GET', 'POST'], // Configura los métodos permitidos
+//     allowedHeaders: '*', // Configura '*' para permitir todos los encabezados
+//   }));
+
+// export default server
+import resolvers from "./graphql/index.js";
+import { GraphQLServer } from "graphql-yoga";
+import path from "path";
 import { fileURLToPath } from 'url';
-import cors from 'cors'
-//------
+
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname( __filename );
-console.log( path.join( __dirname, "graphql/schema.graphql" ))
-//-----
-//relativas    /b17-web
+const __dirname = path.dirname(__filename);
+console.log(path.join(__dirname, "graphql/schema.graphql"));
 
-//absolutas   /c/Users/carlf/Desktop/b17-web
-const server = new GraphQLServer( {
-    typeDefs: path.join( __dirname, "graphql/schema.graphql" ),  // -> /c/Users/carlf/Desktop/b17-web/graphql/schema.graphql //http:heroku-app.com/graphql/schema.graphql"
-    resolvers: resolvers                                                     
-} )
-server.express.use(cors({
-    origin: '*', // Configura aquí los orígenes permitidos, ej. 'http://localhost:3000'
-    methods: ['GET', 'POST'], // Configura los métodos permitidos
-    allowedHeaders: '*', // Configura '*' para permitir todos los encabezados
-  }));
+const allowCrossOrigin = (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
 
-export default server
+const server = new GraphQLServer({
+  typeDefs: path.join(__dirname, "graphql/schema.graphql"),
+  resolvers
+});
+
+server.express.use((req, res, next) => {
+  allowCrossOrigin(req, res, next);
+});
+
+export default server;
