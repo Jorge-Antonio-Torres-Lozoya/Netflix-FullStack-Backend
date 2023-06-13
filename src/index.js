cd9478f368b2a95a894184604bc37ce4fec0dfb2
Jorge-Antonio-Torres-Lozoya <jtorres1@ucol.mx>
2023-06-13 17:27:17 -0600

    fix cors

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 // import server from "./server.js"
- import { connect } from "./database.js";
-import dotenv from 'dotenv';
+//  import { connect } from "./database.js";
+// import dotenv from 'dotenv';
 // // import cors from 'cors';
 
 // server.start( { port: 3000}, ( { port } ) => {
 //     console.log('Sever is running on ' + port )
 // })
 
-
+import { connect } from "./database.js";
+import dotenv from 'dotenv';
 import { GraphQLServer } from 'graphql-yoga';
 import express from 'express';
 import { typeDefs } from './graphql/schema.js';
 import resolvers from "./graphql/index.js"
+import cors from 'cors';
 dotenv.config();
  connect();
 
 
 const app = express();
+app.use(cors());
 
 const server = new GraphQLServer({
   typeDefs,
