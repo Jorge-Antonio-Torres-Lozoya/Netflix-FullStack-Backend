f0de7c48433f6aeff4dfb4d3ce3611dd28381922
Jorge-Antonio-Torres-Lozoya <jtorres1@ucol.mx>
2023-06-13 13:49:44 -0600

    fix cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import server from "./server.js"
 import { connect } from "./database.js";
 import dotenv from 'dotenv';
-import cors from 'cors';
+// import cors from 'cors';
 dotenv.config();
 connect();
 
 // Habilitar CORS
-server.use( cors() );
+// server.use( cors() );
 server.start( { port: 3000}, ( { port } ) => {
     console.log('Sever is running on ' + port )
 })
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import resolvers from "./graphql/index.js"
 import { GraphQLServer} from "graphql-yoga";
 import path from "path"
 import { fileURLToPath } from 'url';
-
+import cors from 'cors'
 //------
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname( __filename );
@@ -15,5 +15,6 @@ const server = new GraphQLServer( {
     typeDefs: path.join( __dirname, "graphql/schema.graphql" ),  // -> /c/Users/carlf/Desktop/b17-web/graphql/schema.graphql //http:heroku-app.com/graphql/schema.graphql"
     resolvers: resolvers                                                     
 } )
+server.express.use(cors());
 
 export default server
